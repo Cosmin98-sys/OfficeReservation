33204a109c8b8f3d0197faca5f4b431c496a5391
Ciobanu Cosmin <ciobanu.cosmin998@gmail.com>
2026-04-09 19:29:19 +0300

    Init commit

---
+++ notification-service/src/main/java/com/officereservation/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.officereservation.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+    static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
